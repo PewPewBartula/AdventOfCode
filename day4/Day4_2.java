68398ad305cc2d9f36868d61e413f7d1e1dd3f22
Bartosz Szymczak <63607789+PewPewBartula@users.noreply.github.com>
2022-02-10 16:42:44 +0100

    Second task of fourth day

---
+++ day4/Day4_2.java
@@ -0,0 +1,75 @@
+package day4;
+
+import java.util.*;
+import day4.Bingo;
+import java.io.File;  
+import java.io.FileNotFoundException;  
+import java.util.Scanner; 
+
+public class Day4_2 {
+	
+	private static final int size = 5;
+	private static List<Bingo> bingos = new ArrayList<Bingo>();
+	private static String[] bingoData;
+	private static String[] numbers;
+	private static int lines = 0;
+	
+	private static void readFromFile(String filename) {
+		
+		String data = "";
+		String numData = "";
+		
+		try {
+		      File myObj = new File(filename);
+		      Scanner myReader = new Scanner(myObj);
+		      while (myReader.hasNextLine()) {
+		    	if(lines == 0) {
+		    		numData += myReader.nextLine();
+		    		numbers = numData.split("\\W+");
+		    		lines += 1;
+		    	}
+		    	else {
+		    		data += myReader.nextLine();
+		    		data += "\n";
+		    	}
+		      }
+		      myReader.close();
+		    } catch (FileNotFoundException e) {
+		      System.out.println("An error occurred");
+		      e.printStackTrace();
+		    }
+		
+		bingoData = data.trim().split("\\W+");
+		int numberOfBingos = bingoData.length/25;
+		int index = 0;
+		for(int i = 0; i < numberOfBingos; i++) {
+			Bingo b = new Bingo();
+			for(int j = 0; j < size; j++ ) {
+				for(int k = 0; k < size; k++) {
+					b.bingo[j][k] = bingoData[index];
+					index += 1; 
+				}
+			}
+			bingos.add(b);
+		}
+	}
+
+	
+	public static void main(String[] args) {
+		readFromFile("data/input.txt");
+		Bingo lastWinner = new Bingo();
+		for(int i = 0; i < numbers.length; i++) {
+			List<Bingo> toRemove = new ArrayList();
+			for(Bingo b : bingos) {
+				b.update(numbers[i]);
+				if(b.winner) {
+					lastWinner = b;
+					toRemove.add(b);
+				}
+			}
+			bingos.removeAll(toRemove);
+		}
+		System.out.println(lastWinner.countWinner());
+	}
+	
+}
